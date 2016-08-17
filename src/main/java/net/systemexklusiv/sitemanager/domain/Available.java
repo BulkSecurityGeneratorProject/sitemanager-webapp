7a9f733121916d09452b250dbcf7050d917471ca
david rival <david.rival@tribejagd.com>
2016-08-17 17:14:16 +0200

    extends domain object

--- src/main/java/net/systemexklusiv/sitemanager/domain/Available.java
+++ src/main/java/net/systemexklusiv/sitemanager/domain/Available.java
@@ -31,6 +31,21 @@ public class Available implements Serializable {
     @Column(name = "end_of_availability", nullable = false)
     private LocalDate endOfAvailability;
 
+    @NotNull
+    @Size(min = 3, max = 20)
+    @Column(name = "country", length = 20, nullable = false)
+    private String country;
+
+    @NotNull
+    @Size(min = 0, max = 20)
+    @Column(name = "city", length = 20, nullable = false)
+    private String city;
+
+    @NotNull
+    @Size(min = 3, max = 20)
+    @Column(name = "postalcode", length = 20, nullable = false)
+    private String postalcode;
+
     @ManyToOne
     private User user;
 
@@ -56,6 +71,30 @@ public class Available implements Serializable {
 
     public void setEndOfAvailability(LocalDate endOfAvailability) {
         this.endOfAvailability = endOfAvailability;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
     }
 
     public User getUser() {
@@ -92,6 +131,9 @@ public class Available implements Serializable {
             "id=" + id +
             ", beginOfAvailability='" + beginOfAvailability + "'" +
             ", endOfAvailability='" + endOfAvailability + "'" +
+            ", country='" + country + "'" +
+            ", city='" + city + "'" +
+            ", postalcode='" + postalcode + "'" +
             '}';
     }
 }
